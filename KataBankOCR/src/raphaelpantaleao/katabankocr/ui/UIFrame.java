078fca77b9d0f4ea221a0268ca8a9ff37421b81a
Raphael Pantaleão <raphaelpanta@gmail.com>
2014-10-14 16:33:22 -0300

    UI remade.

--- KataBankOCR/src/raphaelpantaleao/katabankocr/ui/UIFrame.java
+++ KataBankOCR/src/raphaelpantaleao/katabankocr/ui/UIFrame.java
@@ -1,5 +1,7 @@
 package raphaelpantaleao.katabankocr.ui;
 
+import static java.awt.FlowLayout.CENTER;
+import static java.awt.FlowLayout.LEFT;
 import static raphaelpantaleao.katabanckocr.appconstants.Constants.INPUT_LABEL_NAME;
 import static raphaelpantaleao.katabanckocr.appconstants.Constants.INPUT_LABEL_TEXT;
 import static raphaelpantaleao.katabanckocr.appconstants.Constants.INPUT_TEXT_AREA_NAME;
@@ -15,8 +17,8 @@ import static raphaelpantaleao.katabanckocr.appconstants.Constants.SCANNER_BUTTO
 import static raphaelpantaleao.katabanckocr.appconstants.Constants.SELECT_FILE_BUTTON_NAME;
 import static raphaelpantaleao.katabanckocr.appconstants.Constants.SELECT_FILE_BUTTON_TEXT;
 import static raphaelpantaleao.katabanckocr.builders.JButtonBuilder.newButton;
-import static raphaelpantaleao.katabanckocr.builders.JLabelBuilder.newJLabel;
 import static raphaelpantaleao.katabanckocr.builders.JTextAreaBuilder.newTextArea;
+import static raphaelpantaleao.katabanckocr.builders.JLabelBuilder.newJLabel;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -24,14 +26,20 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class UIFrame extends JFrame {
 
 	private static final long serialVersionUID = 4752440571780079564L;
-	private final JTextArea outputTextArea;
-	private final JTextArea inputTextArea;
+	private final JTextArea outputTextArea = newTextArea()
+			.named(OUTPUT_TEXT_AREA_NAME).withRows(MAX_ACCOUNT_LINES)
+			.withColumns(MAX_ACCOUNT_LINE_LENGTH).nonEditable().build();
+	private final JTextArea inputTextArea = newTextArea()
+			.named(INPUT_TEXT_AREA_NAME).withRows(MAX_SCANNER_LINES)
+			.withColumns(MAX_SCANNER_LINE_LENGTH).nonEditable().build();
 
 	private final JButton scannerButton = newButton()
 			.named(SCANNER_BUTTON_NAME).withText(SCANNER_BUTTON_TEXT)
@@ -44,19 +52,36 @@ public class UIFrame extends JFrame {
 	public UIFrame(String name, String title) {
 		configFrame(name, title);
 
-		inputTextArea = newTextArea().named(INPUT_TEXT_AREA_NAME).withRows(MAX_SCANNER_LINES)
-				.withColumns(MAX_SCANNER_LINE_LENGTH).nonEditable().build();
-		outputTextArea = newTextArea().named(OUTPUT_TEXT_AREA_NAME).withRows(MAX_ACCOUNT_LINES)
-				.withColumns(MAX_ACCOUNT_LINE_LENGTH).nonEditable().build();
+		JPanel panel1 = new JPanel(new FlowLayout(LEFT));
+		JLabel inputLabel = newJLabel().named(INPUT_LABEL_NAME)
+				.withText(INPUT_LABEL_TEXT).build();
+		inputLabel.setPreferredSize(new Dimension(100, 30));
+		panel1.add(inputLabel);
+		panel1.add(selectFileButton);
+		panel1.setPreferredSize(new Dimension(700, 35));
+		add(panel1);
 
-		add(newJLabel().named(INPUT_LABEL_NAME).withText(INPUT_LABEL_TEXT)
-				.build());
-		add(selectFileButton);
-		add(new JScrollPane(inputTextArea));
-		add(newJLabel().named(OUTPUT_LABEL_NAME).withText(OUTPUT_LABEL_TEXT)
-				.build());
-		add(scannerButton);
-		add(new JScrollPane(outputTextArea));
+		JPanel panel2 = new JPanel(new FlowLayout(LEFT));
+		JScrollPane scrollablePane = new JScrollPane(inputTextArea);
+		scrollablePane.setPreferredSize(new Dimension(700, 200));
+		panel2.add(scrollablePane);
+		panel2.setPreferredSize(new Dimension(700, 200));
+		add(panel2);
+
+		JPanel panel3 = new JPanel(new FlowLayout(LEFT));
+		JLabel outputLabel = newJLabel().named(OUTPUT_LABEL_NAME)
+				.withText(OUTPUT_LABEL_TEXT).build();
+		outputLabel.setPreferredSize(new Dimension(100, 30));
+		panel3.add(outputLabel);
+		panel3.add(scannerButton);
+		panel3.setPreferredSize(new Dimension(700, 35));
+		add(panel3);
+
+		JPanel panel4 = new JPanel(new FlowLayout(LEFT));
+		JScrollPane scrollableOutputPane = new JScrollPane(outputTextArea);
+		scrollableOutputPane.setPreferredSize(new Dimension(700, 200));
+		panel4.add(scrollableOutputPane);
+		add(panel4);
 
 		pack();
 	}
@@ -64,9 +89,9 @@ public class UIFrame extends JFrame {
 	private void configFrame(String name, String title) {
 		setName(name);
 		setTitle(title);
-		setLayout(new FlowLayout());
+		setLayout(new FlowLayout(CENTER));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(350, 300));
+		setPreferredSize(new Dimension(800, 600));
 	}
 
 	public void addSelectFileListener(ActionListener listener) {
