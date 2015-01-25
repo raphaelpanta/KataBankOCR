84bbad7c9a7da1cf53178f25d46ff525571f4e4b
Raphael Pantaleão <raphaelpanta@gmail.com>
2015-01-25 11:19:50 -0300

    Performance test on DocumentProcessoTest.

--- KataBankOCR/test/raphaelpantaleao/bankocr/tests/integration/DocumentProcessorTest.java
+++ KataBankOCR/test/raphaelpantaleao/bankocr/tests/integration/DocumentProcessorTest.java
@@ -1,15 +1,19 @@
 package raphaelpantaleao.bankocr.tests.integration;
 
+import static java.lang.System.currentTimeMillis;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
 import static raphaelpantaleao.bankocr.tests.StreamCreator.createStreamOf;
 import static raphaelpantaleao.bankocr.tests.TestConstants.ACCOUNTS_EXPECTED;
 import static raphaelpantaleao.bankocr.tests.TestConstants.NUMBER_IN_DIGITS;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -26,16 +30,34 @@ public class DocumentProcessorTest {
 	@Test
 	public void wellformedDigitsConvertsSuccessfully()
 			throws DocumentProcessorException {
-		docProcessor.process(aStream());
+		docProcessor.process(a10EntryStream());
 		assertThat(docProcessor.entries(),
 				is(equalTo(concat(ACCOUNTS_EXPECTED))));
+	}
+
+	@Test
+	public void documentProcessorCanHandle5000entryFile()
+			throws DocumentProcessorException {
+		Long startTime = currentTimeMillis();
+		docProcessor.process(a5000EntryStream());
+		Long endTime = currentTimeMillis() - startTime;
+		assertThat(endTime, lessThan(500L));
+	}
+
+	private InputStream a10EntryStream() {
+		return createStreamOf(NUMBER_IN_DIGITS);
+	}
+
+	private InputStream a5000EntryStream() {
+		List<String> list = new ArrayList<String>(500);
+		for (int i = 0; i < 500; i++) {
+			list.addAll(asList(NUMBER_IN_DIGITS));
+		}
+		return createStreamOf(list.toArray(new String[] {}));
 	}
 
 	private String concat(String[] strings) {
 		return asList(strings).stream().collect(joining());
 	}
 
-	private InputStream aStream() {
-		return createStreamOf(NUMBER_IN_DIGITS);
-	}
 }
